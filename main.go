05911cbe895bffe3544bbbb3db6a4d8576264f29
Sergey Mayorov <s.mayorov@automacon.ru>
2024-06-22 22:43:14 +0300

    hotfix 2

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 
 func main() {
 	n := ""
-	fmt.Println("feature")
-	fmt.Print("Введите целое число: ")
+	fmt.Println("hotfix")
+	fmt.Print("Введите данные: ")
 	_, err := fmt.Scanf("%s\n", &n)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Вы ввели число: %s\n", n)
+	fmt.Printf("Вы ввели следующие данные: %s\n", n)
 }
