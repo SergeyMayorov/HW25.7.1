7c95145f8c65f16076aab15ecb9834b95abb4a17
Sergey Mayorov <s.mayorov@automacon.ru>
2024-06-22 22:21:22 +0300

    develop

--- main.go
+++ main.go
@@ -6,11 +6,12 @@ import (
 )
 
 func main() {
-	n := 0
+	n := ""
+	fmt.Println("develop")
 	fmt.Print("Введите целое число: ")
-	_, err := fmt.Scan(&n)
+	_, err := fmt.Scanf("%s\n", &n)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Вы ввели число: %d\n", n)
+	fmt.Printf("Вы ввели число: %s\n", n)
 }
